59474cdafb4d97e99803d4df72d6ca9ca55b3265
Mark Tarnavskyi <mark.tarnavskyy@gmail.com>
2025-01-18 00:26:31 +0000

    Initialisation

--- chess-tournament-manager/src/components/Layout.js
+++ chess-tournament-manager/src/components/Layout.js
@@ -4,17 +4,50 @@ import { Outlet, useNavigate } from "react-router-dom";
 
 // Functional component
 function Layout() {
-  
+    
+    //variable
+    const navigate = useNavigate();
+    //function that navigates to login page
+    const clickToLogin = () => {
+        navigate("/login");
+    }
+
   //Display contents
   return (
     <div>
         <header>
             {/*Header*/}
+            <div>
+                {/*Places Logo image*/}
+                <img src={require('../images/test_logo.png')} alt="Logo"/>
+           </div>
+           <div>
+                {/*Places Log In button*/}
+                <button onClick={clickToLogin}>Log In</button>
+                <hr></hr>
+           </div>
         </header>
         {/*Wrapped around*/}
         <main><Outlet /></main>
         <footer>
             {/*Footer*/}
+            <div>
+                <hr></hr>
+                {/*Places Logo image*/}
+                <img src={require('../images/test_logo.png')} alt="Logo"/>
+           </div>
+
+           <div>
+                {/*Contant Us Email*/}
+                <a href="mailto:mark.tarnavskyy@gmail.com">Contact Us</a>
+                {/*Try Service button - to be enabled later*/}
+                <button>Try Service</button>
+           </div>
+
+           <div>
+                {/*Other Information - to be added later*/}
+                <p>*to be filled later*</p>
+           </div>
         </footer>   
     </div>
   );
@@ -23,10 +56,3 @@ function Layout() {
 //Export to be used by App.js
 export default Layout;
 
-//<div>
-//                {/*Logo*/}
-//                <img src={require('../images/test_logo.png')} alt="Logo"/>
-//           </div>
-//           <div>
-//                <button>Log In</button>
-//           </div>
